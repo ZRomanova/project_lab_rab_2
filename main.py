b9adffe78fe22d660f8c127b3ab20401049c89c6
ZRomanova <romanova.zoya.2002@mail.ru>
2022-09-13 12:11:02 +0300

    second commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-height = float(input("Введите рост: "))
-weight = float(input("Введите вес: "))
-steps = float(input("Колличество шагов: "))
-time = float(input("Время: "))
+height = float(input("Введите рост в метрах: "))
+weight = float(input("Введите вес в килограммах: "))
+steps = float(input("Количество шагов: "))
+time = float(input("Время в минутах: "))
 steplength = height /+ 0.37
 length = steps * steplength  
 lengthk = length / 1000
 speed = length / (time * 60)
-E = 0.035 * weight + (speed ** 2 / height) * 0.029 * weight 
-kall = E * time 
+e = 0.035 * weight + (speed ** 2 / height) * 0.029 * weight 
+kall = e * time 
 print("Пройденная дистанция в метрах:", lengthk * 1000)
 print("Количество сожженных калорий:", kall)
 print("Пройденная дистанция в киллометрах", lengthk)
